97e2861e373d9cd829ab0c19ccd141a2a9525e84
Martin Scholl <martin.scholl@cismet.de>
2015-03-30 23:03:41 +0200

    initial vehicle diagrams from ooi

--- app/scripts/directives/pilotELinechart.js
+++ app/scripts/directives/pilotELinechart.js
@@ -25,12 +25,12 @@ angular.module(
                     // we simply assume same repo for all ws thus read everything in one single operation instead of
                     // per dataitem, etc
                     setValues = function () {
-                        var res, allEntities, allOoiWs, ooiWs, t0 ;
+                        var res, allEntities, allOoiWs, ooiWs;
                         
                         res = $resource(ICMM_API + '/CRISMA.worldstates/' + $scope.worldstateId + '?deduplicate=true&omitNullValues=false');
                         
                         res.get({}, function(ws) {
-                            var createWsChain, incidentAreaWkt, ooiEntityRes, ooiWsChain, ooiWsRes, vehicleRequestEntities;
+                            var createWsChain, incidentAreaWkt, ooiEntityRes, ooiWsChain, ooiWsRes, getMinutes;
                             
                             // hardcoded repo
                             ooiEntityRes = $resource('https://crisma-pilote.ait.ac.at/api/entity?wsId=:wsId');
@@ -70,6 +70,15 @@ angular.module(
                                 p = ooiWsRes.get({wsId: wsId}).$promise;
                                 allOoiWs.push(p);
                             });
+                            
+                            getMinutes = function (index) {
+                                var ws0, wsN;
+                                
+                                ws0 = ooiWs[0];
+                                wsN = ooiWs[index];
+                                
+                                return parseInt((Date.parse(wsN.dateTime) - Date.parse(ws0.dateTime)) / (60 * 1000));
+                            };
                             
                             $q.all(allOoiWs).then(function(ws) {
                                 ooiWs = ws;
@@ -117,7 +126,7 @@ angular.module(
                                 });
 
                                 $q.all(allEntities).then(function(res) {
-                                    var incidentPolygon;
+                                    var i, incidentPolygon;
                                     
                                     incidentPolygon = new Terraformer.Polygon(Terraformer.WKT.parse(incidentAreaWkt));
                                     
@@ -125,15 +134,17 @@ angular.module(
                                         return a.x - b.x;
                                     });
                                     
-                                    
+                                    i = 0;
                                     res.forEach(function (entities) {
-                                        var onSiteCount, alarmedCount, onTheWayCount;
+                                        var onSiteCount, alarmedCount, onTheWayCount, minutes;
                                         
                                         onSiteCount = alarmedCount = onTheWayCount = 0;
                                         
                                         // the entities should be sorted like the ws chain, t(0) to t(n)
                                         entities.forEach(function (entity) {
-                                            var entityPos;
+                                            var entityPos, onTheWay, point;
+                                            
+                                            onTheWay = false;
                                             if(entity.entityTypeId === 7) {
                                                 // assume 7 === vehicle
                                                 
@@ -143,21 +154,59 @@ angular.module(
                                                         entity.entityInstancesGeometry[0].geometry.geometry &&
                                                         entity.entityInstancesGeometry[0].geometry.geometry.wellKnownText) {
                                                     entityPos = Terraformer.WKT.parse(entity.entityInstancesGeometry[0].geometry.geometry.wellKnownText);
+                                                    // some gui messed up the coordinate order oO, trying to fix this crap here
+                                                    point = incidentPolygon.coordinates[0][0];
+                                                    
+                                                    if(Math.abs(point[0] - entityPos.coordinates[0]) >
+                                                            Math.abs(point[0] - entityPos.coordinates[1])) {
+                                                        // it is very likely that the coordinates are swapped
+                                                        point = entityPos.coordinates[0];
+                                                        entityPos.coordinates[0] = entityPos.coordinates[1];
+                                                        entityPos.coordinates[1] = point;
+                                                    }
                                                 }
                                                 
-                                                if(entityPos && Terraformer.Tools.polygonContainsPoint(incidentPolygon, entityPos.coordinates)) {
+                                                if(entityPos && Terraformer.Tools.polygonContainsPoint(incidentPolygon.coordinates, entityPos.coordinates)) {
                                                     // vehicle on site
                                                     onSiteCount++;
                                                 } else {
                                                     // not on site
+                                                    
                                                     entity.entityInstancesProperties.forEach(function(p) {
                                                         if (p.entityTypePropertyId === 1000) {
                                                             // assume resource command
+                                                            if(p.entityPropertyValue.indexOf('Treat') >= 0 ||
+                                                                    p.entityPropertyValue.indexOf('Rescue') >= 0 ||
+                                                                    p.entityPropertyValue.indexOf('Evacuate') >= 0 ||
+                                                                    p.entityPropertyValue.indexOf('Dispatch') >= 0) {
+                                                                onTheWayCount++;
+                                                                onTheWay = true;
+                                                            }
                                                         }
                                                     });
+                                                    
+                                                    if(onTheWay) {
+                                                        entity.entityInstancesProperties.forEach(function(p) {
+                                                            var v;
+                                                            if (p.entityTypePropertyId === 312) {
+                                                                // assume resource command
+                                                                v = parseInt(p.entityPropertyValue);
+                                                                if(v === 0 || v === -1) {
+                                                                    alarmedCount++;
+                                                                }
+                                                            }
+                                                        });
+                                                    }
                                                 }
                                             }
                                         });
+                                        
+                                        minutes = getMinutes(i);
+                                        vOnSite.push({x: minutes, y: onSiteCount});
+                                        vOnTheWay.push({x: minutes, y: onTheWayCount});
+                                        vAlarmed.push({x: minutes, y: alarmedCount});
+                                        
+                                        i++;
                                     });
                                });
                             });
